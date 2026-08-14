c9f42ddf692baadeec63bcdef04f9da8677942a0
diaup20 <148984593+diaup20@users.noreply.github.com>
2026-08-14 22:58:36 +0300

    New updated

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,18 +2,9 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getAnalytics, isSupported } from 'firebase/analytics';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDUjbhIjL5qlZbcjfoBluGDL-E1BrWXU30",
-  authDomain: "almahal-361ce.firebaseapp.com",
-  projectId: "almahal-361ce",
-  storageBucket: "almahal-361ce.firebasestorage.app",
-  messagingSenderId: "291472540901",
-  appId: "1:291472540901:web:850fe195e7c259f24b400c",
-  measurementId: "G-LKQN8TT5RC"
-};
+import firebaseConfig from '../../firebase-applet-config.json';
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-export const db = getFirestore(app);
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const auth = getAuth(app);
 export const analytics = typeof window !== 'undefined' ? isSupported().then(yes => yes ? getAnalytics(app) : null) : null;
